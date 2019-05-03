f65502e5568fe37ad3b216b068def4b5344dd45b
Josh Morris <jmo0624@gmail.com>
2019-05-03 00:33:42 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -44,6 +44,12 @@ end
 
 def valid_phone_number?(phone)
   
-  if phone.match(/\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})
+  if phone.match(/\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})/) == nil
+    
+    return false
+    
+  else
+    
+    return true
 
 end
